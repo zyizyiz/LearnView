6e50ba35f10017c1212d70c276abd40b83da5f57
zyizyiz <>
2019-01-08 22:08:50 +0800

    project init

---
+++ LearnView/LearnView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnView
+//
+//  Created by ios on 2019/1/8.
+//  Copyright © 2019年 KN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnView/LearnView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnView
+//
+//  Created by ios on 2019/1/8.
+//  Copyright © 2019年 KN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
